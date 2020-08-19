88a4839ed4f39c62d0c0a440fd491000bb01199c
ElMehdiAbbes <Mahdix.abbes@gmail.com>
2020-08-19 13:08:28 +0100

    Foundation commit

---
+++ SignalRCore/Hubs/ChatHub.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SignalRCore.Hubs;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public static ConnectionMapping<string> connections = new ConnectionMapping<string>();
+
+        public void Dummy(string user)
+        {
+            string key = user;
+            connections.Add(key, Context.ConnectionId);
+        }
+        public async Task SendMessage(string user, string message)
+        {
+            HashSet<string> connectedUsers = new HashSet<string>();
+            string key = user;
+            foreach (var connectionId in connections.GetConnections(key))
+            {
+                connectedUsers.Add(connectionId);
+            }
+            // Send Messages to all users
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            // Send messages to specific users
+            // await Clients.Clients(connectedUsers.ToList()).SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
---
+++ SignalRCore/wwwroot/js/chat.js
@@ -0,0 +1,41 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+//Disable send button until connection is established
+document.getElementById("sendButton").disabled = true;
+
+connection.on("ReceiveMessage", function (user, message) {
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var encodedMsg = user + " says " + msg;
+    var li = document.createElement("li");
+    li.textContent = encodedMsg;
+    document.getElementById("messagesList").appendChild(li);
+});
+
+document.getElementById("connectButton").addEventListener("click", function (event) {
+    connection.start().then(function () {
+        var user = document.getElementById("userInput").value;
+        connection.invoke("Dummy", user)
+            .then(function () {
+                document.getElementById("sendButton").disabled = false;
+                document.getElementById("userInput").disabled = true;
+                document.getElementById("connectButton").disabled = true;
+            })
+            .catch(function (err) {
+                return console.error(err.toString());
+            });
+    }).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+    connection.invoke("SendMessage", user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
